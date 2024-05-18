55b716595eb8d3e9d402552dd2d12a3ff94c9794
Viery Nugroho <viery15102002@gmail.com>
2024-05-18 07:56:29 +0700

    add: error handler, logger, ejs, & root route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-const airplane = require("./airplane");
+const airplane = require('./airplane');
 
-router.use("/api/v1/airplane", airplane);
+router.get('/api/v1', (req, res, next) => {
+	res.status(200).json({
+		status: true,
+		message: 'Welcome to API skyfly',
+	});
+});
+
+router.use('/api/v1/airplane', airplane);
 
 module.exports = router;
