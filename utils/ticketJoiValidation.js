d284f3f0c45363ba6a4c8f74a5bc7efaeca3b394
LowisHutabarat <lowishutabarat0612@gmail.com>
2024-05-27 19:08:16 +0700

    Include other table and fixing some errors like validation and added other table to Joi

--- utils/ticketJoiValidation.js
+++ utils/ticketJoiValidation.js
@@ -2,21 +2,22 @@ const Joi = require("joi").extend(require("@joi/date"));
 
 //ticket
 const TicketSchema = Joi.object({
+    flightId: Joi.string().required(),
+    userId: Joi.string().required(),
+    seatId: Joi.string().required(),
     bookingDate: Joi.date()
-
         .required()
         .greater(Date.now() - 24 * 60 * 60 * 1000),
     // bookingDate: Joi.date()
     //     .required()
     //     .greater(Date.now() - 24 * 60 * 60 * 1000),
     price: Joi.number().positive().required(),
-    status: Joi.string().valid("PENDING", "SUCCESS", "FAILED").required(),
 });
 
 const UpdateTicketSchema = Joi.object({
-    bookingDate: Joi.date().min("now"),
+    code: Joi.string(),
+    bookingDate: Joi.date().greater(Date.now() - 24 * 60 * 60 * 1000),
     price: Joi.number().positive(),
-    status: Joi.string(),
 });
 
 module.exports = { TicketSchema, UpdateTicketSchema };
