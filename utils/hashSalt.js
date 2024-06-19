4dca0147479ee8b5cd33352dda4a341f8a68e381
Viery Nugroho <viery15102002@gmail.com>
2024-06-19 18:27:02 +0700

    fix: delay sending email

--- utils/hashSalt.js
+++ utils/hashSalt.js
@@ -1,12 +1,12 @@
 const bcrypt = require("bcrypt");
 
-const secretHash = (string) => {
+const secretHash = async (string) => {
     const saltRounds = parseInt(process.env.SALT);
     const hashed = bcrypt.hashSync(string, saltRounds);
     return hashed;
 };
 
-const secretCompare = (data, hashed) => {
+const secretCompare = async (data, hashed) => {
     return bcrypt.compareSync(data, hashed);
 };
 
--- utils/jwtGenerate.js
+++ utils/jwtGenerate.js
@@ -1,13 +1,9 @@
 const jwt = require("jsonwebtoken");
 
-const generateJWT = (payload) => {
-    return jwt.sign(
-        payload,
-        process.env.JWT_SIGNATURE_KEY,
-        {
-            expiresIn: process.env.JWT_EXPIRED
-        }
-    )
-}
+const generateJWT = async (payload) => {
+    return jwt.sign(payload, process.env.JWT_SIGNATURE_KEY, {
+        expiresIn: process.env.JWT_EXPIRED,
+    });
+};
 
 module.exports = { generateJWT };
