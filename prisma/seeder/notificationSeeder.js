c0c63ce43c3bb97d7f982abd1d5ce58657b0a8f3
RMYP <160878278+RMYP@users.noreply.github.com>
2024-06-22 20:58:32 +0800

    add: seeder for notifications

---
+++ prisma/seeder/notificationSeeder.js
@@ -0,0 +1,62 @@
+const { PrismaClient } = require("@prisma/client");
+const { secretHash } = require("../../utils/hashSalt");
+const { randomUUID } = require("crypto");
+
+const prisma = new PrismaClient();
+
+async function main() {
+    let notificationsData = [];
+    const notificationType = [
+        {
+            type: "Promotions",
+            title: "Diskon 50% buat kamu, iya kamu 😘",
+            content:
+                "Dapatkan potongan 50% dalam pembelian tiket!, promo ini berlaku untuk semua penerbangan",
+        },
+        {
+            type: "Warning",
+            title: "Pesawat kamu sudah mau berangkat!",
+            content:
+                "Jangan sampai ketinggalan pesawat! ayo buruan Check-in, pesawat kamu akan berangkat sebentar lagi",
+        },
+        {
+            type: "Information",
+            title: "Pemberitahuan penerbangan",
+            content:
+                "Penerbangan anda ke Bali akan segera berangkat 2 jam lagi. ayo buruan ke gerbang keberangkatan",
+        },
+        {
+            type: "Update",
+            title: "Pembaruan Aplikasi",
+            content:
+                "Ada update baru loh buat aplikasi kami. Nikmati fitur baru sekarang!",
+        },
+    ];
+
+        for (const notifications of notificationType) {
+            const data = {
+                id: randomUUID(),
+                type: notifications.type,
+                notificationsTitle: notifications.title,
+                notificationsContent: notifications.content,
+                date: new Date("2030-01-01"),
+            };
+            notificationsData.push(data);
+        }
+    
+    for (const notifications of notificationsData) {
+        await prisma.notifications.create({
+            data: notifications,
+        });
+    }
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
