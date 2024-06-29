39011b07245859cfc1b3c4e35e15802b04c73499
Viery Nugroho <viery15102002@gmail.com>
2024-06-29 11:58:56 +0700

    update: feature update user & hashing data

--- controllers/user.js
+++ controllers/user.js
@@ -43,7 +43,7 @@ const getAllUsers = async (req, res, next) => {
             where: {
                 name: {
                     contains: search,
-                    mode: "insensitive", // Optional: to make the search case insensitive
+                    mode: "insensitive",
                 },
             },
         });
@@ -191,49 +191,19 @@ const updateUser = async (req, res, next) => {
                     role,
                 },
             });
-     // Lakukan transaksi untuk memastikan konsistensi data
-     const updatedUser = await prisma.$transaction(async (tx) => {
-        // Perbarui data pengguna
-        const userUpdate = await tx.user.update({
-            where: { id: userId },
-            data: {
-                name,
-                phoneNumber,
-                familyName,
-                role,
-            },
-        });
 
-        let hashedPassword;
-        let data = {
-            isVerified,
-        };
-        if (password) {
-            hashedPassword = await secretHash(password);
-            data = {
-                password: hashedPassword,
-                isVerified: isVerified,
-            };
-            await prisma.$transaction(async (tx) => {
-                let cekAuth = await tx.auth.getAllUsers({
-                    where: {
-                        userId: userId
-                    }
-                });
-                console.log(cekAuth);
+            if (hashedPassword) {
                 await tx.auth.update({
-                    where: {
-                        userId: userId
+                    where: { userId: userId },
+                    data: {
+                        password: hashedPassword,
+                        isVerified,
                     },
-                    data: data
                 });
-            });
+            }
 
-        }
-
-        return userUpdate;
-    });
-    console.log(res.data);
+            return userUpdate;
+        });
 
         res.status(200).json({
             status: true,
@@ -251,7 +221,6 @@ const updateUser = async (req, res, next) => {
         next(createHttpError(500, { message: error.message }));
     }
 };
-
 
 const deleteUser = async (req, res, next) => {
     try {
